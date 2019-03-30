d852a438e9065000f06a2c234b9f52bab2676a43
Alex Li <7Alex7Li@gmail.com>
2019-03-30 14:20:21 -0400

    Added model

--- kernelRidge.py
+++ kernelRidge.py
@@ -1,14 +1,15 @@
 import normalizer
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import confusion_matrix
 
 train_features, train_labels, test_features, test_labels = normalizer.load_data(
     1)
-decisionTree = DecisionTreeClassifier()
+decisionTree = KernelRidge(kernel='laplacian')
 decisionTree.fit(train_features, train_labels)
 labels = decisionTree.predict(test_features)
-print(labels)
-tn, fp, fn, tp = confusion_matrix(labels, test_labels).ravel();
+
+labels = labels > .5
+tn, fp, fn, tp = confusion_matrix(labels, test_labels).ravel()
 error = ((fp + fn) / (tn + fp + fn + tp)) * 100
 print("true negatives: " + str(tn))
 print("false positives: " + str(fp))
