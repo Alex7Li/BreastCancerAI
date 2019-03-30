609f0cf3c536ba6eddfefa2999c568609a3f0acf
Alex Li <7Alex7Li@gmail.com>
2019-03-30 13:40:35 -0400

    Update data grab to return int labels

--- normalizer.py
+++ normalizer.py
@@ -4,34 +4,49 @@ from sklearn.preprocessing import normalize
 
 
 def load_data(random_seed):
-    my_data = np.genfromtxt('data.csv', delimiter=',', encoding="utf8")
-    # Convert first col to numbers
-    i = 0
+    data = np.genfromtxt('data.csv', delimiter=',', encoding="utf8")
+    # Remove labels
+    data = np.delete(data, (0), axis=0)
+
+    # Randomize Data
+    size = len(data)
+    np.random.seed(random_seed)
+    perm = np.random.permutation(size)
+    data = data[perm]
+    print(size)
+
+    # Split up data
+    train_size = (size * 4) // 5
+    features = data[:, 1:train_size]
+    labels = np.array(data[:, 0:1],dtype=np.int32)
+
+    # normalize data
+    features = normalize(features, axis=0) * np.sqrt(size)
+
+    # read class data
+    i = -2
     with open('data.csv', 'r') as csvfile:
         reader = csv.reader(csvfile, dialect='excel')
         for row in reader:
-            if row[0] == "B":
-                my_data[i][0] = 1
-            else:
-                my_data[i][0] = -1
             i += 1
-    my_data = np.delete(my_data, (0), axis=0)
-    size = len(my_data)
-    my_data = normalize(my_data, axis=0) * np.sqrt(size)
+            if i == -1:
+                continue
+            if row[0] == "B":
+                labels[i] = 1
+            else:
+                labels[i] = 0
+    labels = labels[perm]
 
-    np.random.seed(random_seed)
-    my_data = my_data[np.random.permutation(size)]
-    print(size)
+    # split data
+    train_features = features[:, 1:train_size]
+    test_features = features[:, train_size:size]
+    train_labels = labels[:, 0:1]
+    test_labels = labels[:, 0:1]
 
-    train_size = (size * 4) // 5
-    train_data = my_data[0:train_size]
-    test_data = my_data[train_size:size]
-    np.savetxt("test_data.csv", test_data,delimiter=',')
-    np.savetxt("train_data.csv", train_data,delimiter=',')
-    train_features = train_data[:, 1:train_size]
-    test_features = test_data[:, 1:train_size]
-    train_labels = train_data[:, 0:1]
-    test_labels = test_data[:, 0:1]
+    train_data = np.concatenate((train_labels, train_features), axis=1)
+    test_data = np.concatenate((test_labels, test_features), axis=1)
+    np.savetxt("test_data.csv", test_data, delimiter=',')
+    np.savetxt("train_data.csv", train_data, delimiter=',')
     return train_features, train_labels, test_features, test_labels
 
 
