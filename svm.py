faceea947a00bc5555c5654e22599f841cc8e853
Penny Lee <lee.7953@osu.edu>
2019-03-30 19:01:57 -0400

    svm

--- svm.py
+++ svm.py
@@ -1,18 +1,20 @@
 import normalizer
-from sklearn.kernel_ridge import KernelRidge
+from sklearn import svm
 from sklearn.metrics import confusion_matrix
 
-train_features, train_labels, test_features, test_labels = normalizer.load_data(
-    1)
-decisionTree = KernelRidge(kernel='laplacian')
-decisionTree.fit(train_features, train_labels)
-labels = decisionTree.predict(test_features)
+train_features, train_labels, test_features, test_labels = normalizer.load_data(1, range(30))
+ridge = svm.SVC(kernel='linear')
+ridge.fit(train_features, train_labels)
+labels = ridge.predict(test_features)
 
 labels = labels > .5
 tn, fp, fn, tp = confusion_matrix(labels, test_labels).ravel()
+
+# print results
+print(str(tn) + "\t" + str(fp) + "\t" + str(fn) + "\t" + str(tp))
 error = ((fp + fn) / (tn + fp + fn + tp)) * 100
 print("true negatives: " + str(tn))
 print("false positives: " + str(fp))
 print("false negatives: " + str(fn))
-print("false negatives: " + str(tp))
+print("true positives: " + str(tp))
 print("Percent Error: " + str(error) + "%")
